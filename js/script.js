2f134c487aeee5fc5de247130e5ea11761b6da2e
zsiskos <43017972+zsiskos@users.noreply.github.com>
2020-03-03 12:43:17 -0500

    if tie, deals extra cards to players

--- js/script.js
+++ js/script.js
@@ -85,44 +85,115 @@ function initiateWar() {
 	cardOneElement.setAttribute('src', playerOneCard.imgUrl);
 	let cardTwoElement = document.getElementById('player2Card');
 	cardTwoElement.setAttribute('src', playerTwoCard.imgUrl);
-	// //Winning player keeps both cards
-	// if (playerOneCard.rank > playerTwoCard.rank) {
-	// 	playerDecks[1].push(playerTwoCard)
-	// 	playerDecks[1].push(playerOneCard)
-	// 	playerOneScore = playerDecks[1].length
-	// 	playerTwoScore = playerDecks[-1].length
-	// 	console.log("player1 pt")
-	// } else if (playerOneCard.rank < playerTwoCard.rank) {
-	// 	playerDecks[-1].push(playerTwoCard)
-	// 	playerDecks[-1].push(playerOneCard)
-	// 	playerOneScore = playerDecks[1].length
-	// 	playerTwoScore = playerDecks[-1].length
-	// } else {
-	// 	console.log("tie")
-	// }
-
-	//compare values to award points
+	//Winning player keeps both cards NEW FEATURE
 	if (playerOneCard.rank > playerTwoCard.rank) {
-		playerOneScore += 1
+		playerDecks[1].push(playerTwoCard)
+		playerDecks[1].push(playerOneCard)
+		playerOneScore = playerDecks[1].length
+		playerTwoScore = playerDecks[-1].length
 		console.log("player1 pt")
 	} else if (playerOneCard.rank < playerTwoCard.rank) {
-		playerTwoScore += 1 
-		console.log("player2 pt")
+		playerDecks[-1].push(playerTwoCard)
+		playerDecks[-1].push(playerOneCard)
+		playerOneScore = playerDecks[1].length
+		playerTwoScore = playerDecks[-1].length
 	} else {
-		console.log("tie")
+		return goToWar();
 	}
-	//displays points on screen
-	let playerOneScoreEl = document.getElementById('score1');
-	playerOneScoreEl.innerText = playerOneScore
-	let playerTwoScoreEl = document.getElementById('score2');
-	playerTwoScoreEl.innerText = playerTwoScore
-	// check if there are any cards left to play
-	if (playerDecks[1].length === 0) {
-		console.log('time to check winner')
-		fight.remove();
-		checkWinner();
-	}
-};
+}
+		//tie function to go to war
+		// change header to "go to war" in red, and display tie arena, change button to read "go to war" IS THIS NEEDED?
+		// if doin button, then player must hit to activate
+		//deal 3 cards from player1 deck to tie arena and 1 to play area, do same for player2
+		// whoever win, gets all the cards
+
+function goToWar() {
+	//get card from playerDecks for player1
+	let playerOneCard = playerDecks[1].shift()
+	let playerOneTie1 = playerDecks[1].shift()
+	let playerOneTie2 = playerDecks[1].shift()
+	let playerOneTie3 = playerDecks[1].shift()
+
+	//get card from playerDecks for player2
+	let playerTwoCard = playerDecks[-1].shift()
+	let playerTwoTie1 = playerDecks[-1].shift()
+	let playerTwoTie2 = playerDecks[-1].shift()
+	let playerTwoTie3 = playerDecks[-1].shift()
+
+	//player 1 change to show image related to card above
+	let cardOneElement = document.getElementById('player1Card');
+	cardOneElement.setAttribute('src', playerOneCard.imgUrl);
+
+	let playerOnetie1El = document.getElementById('player1tie1');
+	playerOnetie1El.setAttribute('src', playerOneTie1.imgUrl);
+	
+	let playerOneTie2El = document.getElementById('player1tie2');
+	playerOneTie2El.setAttribute('src', playerOneTie2.imgUrl);
+	
+	let playerOneTie3El = document.getElementById('player1tie3');
+	playerOneTie3El.setAttribute('src', playerOneTie3.imgUrl);
+
+	//player 2 change to show image related to card above
+	let cardTwoElement = document.getElementById('player2Card');
+	cardTwoElement.setAttribute('src', playerTwoCard.imgUrl);
+
+	let playerTwoTie1El = document.getElementById('player2tie1');
+	playerTwoTie1El.setAttribute('src', playerTwoTie1.imgUrl);
+	
+	let playerTwoTie2El = document.getElementById('player2tie2');
+	playerTwoTie2El.setAttribute('src', playerTwoTie2.imgUrl);
+	
+	let playerTwoTie3El = document.getElementById('player2tie3');
+	playerTwoTie3El.setAttribute('src', playerTwoTie3.imgUrl);
+}	
+	
+// 	//change to show image related to card above
+	
+	
+	
+// 	let cardTwoElement = document.getElementById('player2Card');
+// 	cardTwoElement.setAttribute('src', playerTwoCard.imgUrl);
+	
+// 	let playerTwoCard = playerDecks[-1].shift()
+	
+// 	//Winning player keeps both cards NEW FEATURE
+// 	if (playerOneCard.rank > playerTwoCard.rank) {
+// 		playerDecks[1].push(playerTwoCard)
+// 		playerDecks[1].push(playerOneCard)
+// 		playerOneScore = playerDecks[1].length
+// 		playerTwoScore = playerDecks[-1].length
+// 		console.log("player1 pt")
+// 	} else if (playerOneCard.rank < playerTwoCard.rank) {
+// 		playerDecks[-1].push(playerTwoCard)
+// 		playerDecks[-1].push(playerOneCard)
+// 		playerOneScore = playerDecks[1].length
+// 		playerTwoScore = playerDecks[-1].length
+// 	} else {
+
+// 	}
+
+// 	//compare values to award points
+// 	if (playerOneCard.rank > playerTwoCard.rank) {
+// 		playerOneScore += 1
+// 		console.log("player1 pt")
+// 	} else if (playerOneCard.rank < playerTwoCard.rank) {
+// 		playerTwoScore += 1 
+// 		console.log("player2 pt")
+// 	} else {
+// 		console.log("tie")
+// 	}
+// 	//displays points on screen
+// 	let playerOneScoreEl = document.getElementById('score1');
+// 	playerOneScoreEl.innerText = playerOneScore
+// 	let playerTwoScoreEl = document.getElementById('score2');
+// 	playerTwoScoreEl.innerText = playerTwoScore
+// 	// check if there are any cards left to play
+// 	if (playerDecks[1].length === 0) {
+// 		console.log('time to check winner')
+// 		fight.remove();
+// 		checkWinner();
+// 	}
+// };
 
 //Comparing scores to find a winner
 function checkWinner() {
@@ -132,7 +203,7 @@ function checkWinner() {
 		document.getElementById('p2Name').insertAdjacentText("beforeend", ` won the war.`)
 	} else {
 		document.getElementById('p1Name').insertAdjacentText("beforeend", ` tied with Player 2`)
-		document.getElementById('p2Name').insertAdjacentText("beforeend", ` tied with Player 2`)
+		document.getElementById('p2Name').insertAdjacentText("beforeend", ` tied with Player 1`)
 	}
 }
 
