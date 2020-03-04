af1ca3e332da74a42c8f8f5856c46977bc4379f8
zsiskos <43017972+zsiskos@users.noreply.github.com>
2020-03-04 16:56:37 -0500

    add modal for instructions

--- js/script.js
+++ js/script.js
@@ -236,3 +236,30 @@ function renderTieArena () {
 	playerTwoTie3El.setAttribute('src', "images/backs/blue.svg")
 	}
 }
+
+
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let instructVid = document.getElementById("instructions");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+instructions.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
